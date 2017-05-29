7965106cdf1bd5171eb8cf95623f6dce94209e9a
Graig <Graig>
2017-05-29 08:29:49 -0500

    cleaning up content

--- 06.fizz-buzz/js/script.js
+++ 06.fizz-buzz/js/script.js
@@ -1,6 +1,6 @@
 // var numberString = '';
 function FizzBuzz() {
-  for (var i = 1; i < 101; i++) {
+  for (var i = 1; i < 201; i++) {
     if (i % 15 === 0) {  //3 * 5 = 15.  3 and 5 are primes as well.  Could use (i%3 === 0 && i&5 === 0)
       // console.log('FizzBuzz');
       // numberString += ' FizzBuzz ';
